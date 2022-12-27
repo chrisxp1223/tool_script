72f5b289078a135dbff03a6cc583f5b932a1cb2a
Chris.Wang <chris.wang@amd.corp-partner.google.com>
2022-12-27 13:50:58 +0800

    update suspend_test
    change the log location.
    add suspend_min/suspend_max
    
    Signed-off-by: Chris.Wang <chris.wang@amd.corp-partner.google.com>

--- suspend_test.sh
+++ suspend_test.sh
@@ -8,7 +8,8 @@ C_DODGERBLUE2="\033[38;5;27m"
 NC='\033[0m' # No Color
 VERSION="1.0.0"
 
-logfile="$(pwd)/suspend_result-$(date -Iseconds).log"
+logdir="/var/log/suspend_result"
+logfile=$logdir/"suspend_result-$(date -Iseconds)".log
 date -Iseconds > $logfile
 
 
@@ -43,7 +44,8 @@ help_menu () {
     echo "Commands:"
     echo "  v : suspend script version"
     echo "  w : wake up delay time"
-    echo "  s : suspend delay time"
+    echo "  a : suspend delay time"
+    echo "  b : suspend delay time"
     echo "  c : cycles [defalut : 2500]"
     echo "  h : help"
      exit 1
@@ -60,38 +62,57 @@ suspend_test_main () {
     crossystem | grep "fwid" >> $logfile 
     echo "" >> $logfile
     echo "" >> $logfile
-    echo ""
-    _printf_color "${RED}" "Suspend stress testing...... press ctrl + c to stop "
-    suspend_stress_test -c $1 --wake_min=$2 --suspend_min=$3  >> $logfile 
+    echo "" >> $logfile
 
+    _printf_color "${RED}" "Suspend stress testing...... press ctrl + c to stop "
+    suspend_stress_test -c $1 --wake_min=$2 --suspend_min=$3 --suspend_max=$4 --record_dmesg_dir=$logdir >> $logfile 
+
+
+    cat /sys/kernel/debug/amd_pmc/s0ix_stats >> $logfile
     print_log_end
+    
+
+    echo "" >> $logfile
+    echo "" >> $logfile
+    echo "" >> $logfile
+    echo "#################################" >> $logfile
+    echo "      Suspend test end "         >> $logfile
+    echo "#################################" >> $logfile
 }
 
 
 
 main () {
 
-    local suspend_delay_s="15"
     local wake_delay_s="10"
+    local suspend_min="15"
+    local suspend_max="20"
     local cycles="2500"
     local show_help=""
     local getpot_ret=""
 
-    while getopts 'vw:s:c:h' flag; do
+    while getopts 'vw:c:a:b:h' flag; do
     case ${flag} in
       v) tool_version ;;
       w) wake_delay_s="${OPTARG}" ;;
-      s) suspend_delay_s="${OPTARG}" ;;
+      a) suspend_min="${OPTARG}" ;;
+      b) suspend_max="${OPTARG}" ;;
       c) cycles="${OPTARG}" ;;
       h) help_menu ;;
       *) die "invalid option found" ;;
      esac
     done
 
-    _printf_color "${GREEN}" "suspend_delay_s : ${suspend_delay_s} "
     _printf_color "${GREEN}" "wake_delay_s : ${wake_delay_s} "
+    _printf_color "${GREEN}" "suspend_min : ${suspend_min} "
+    _printf_color "${GREEN}" "suspend_max : ${suspend_max} "
     _printf_color "${GREEN}" "cycles : ${cycles} "
-    suspend_test_main $cycles $wake_delay_s $suspend_delay_s 
+    
+    if [ ! -d "$logdir" ]; then
+        echo "$logdir does exist."
+        mkdir $logdir
+    fi
+    suspend_test_main $cycles $wake_delay_s $suspend_min $suspend_max
     
 }
 
