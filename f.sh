88e9d97fe87d60f68b7afcc456973a50f4309d91
Chris Wang <chris.wang@amd.corp-partner.google.com>
2023-07-18 19:33:22 +0800

    fine tune the f.sh
    Signed-off-by: Chris Wang <chris.wang@amd.corp-partner.google.com>

--- f.sh
+++ f.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
--df
+
+# Initialize variables
+flash=false
+board=""
+file=""
+
 # Parse command line options
 while getopts ":df:b:" opt; do
-  case $opt in
+  case "${opt}" in
     d)
       flash=true
       ;;
     f)
-      file=$OPTARG
+      file="${OPTARG}"
       ;;
     b)
-      board=$OPTARG
+      board="${OPTARG}"
       ;;
     \?)
-      echo "Invalid option: -$OPTARG" >&2
+      echo "Invalid option: -${OPTARG}" >&2
       exit 1
       ;;
     :)
-      echo "Option -$OPTARG requires an argument." >&2
+      echo "Option -${OPTARG} requires an argument." >&2
       exit 1
       ;;
   esac
 done
 
-# Determine file name based on -d option
-if [ "$flash" = true ]; then
-  file="/build/$board/firmware/image-$board.serial.bin"
-else
-  file="/build/$board/firmware/image-$board.bin"
+# Check if board name is provided
+if [[ -z "${board}" ]]; then
+  echo "Board name is required. Use -b option to specify the board name." >&2
+  exit 1
+fi
+
+# Check if file exists
+if [[ -z "${file}" ]]; then
+  if [[ "${flash}" = true ]]; then
+    file="/build/${board}/firmware/image-${board}.serial.bin"
+  else
+    file="/build/${board}/firmware/image-${board}.bin"
+  fi
+  if [[ ! -f "${file}" ]]; then
+    echo "File ${file} does not exist." >&2
+    exit 1
+  fi
 fi
 
 # Increase the coreboot file size
-sudo truncate $file --size 33554432
+sudo truncate "${file}" --size 33554432
 
-# Flash the coreboot .bin file if -d option is specified
-if [ "$flash" = true ]; then
-  sudo futility update --servo --wp=0 -v -d -i $file
-fi
+# Flash the coreboot .bin.
+sudo futility update --servo --wp=0 -v -i "${file}"
