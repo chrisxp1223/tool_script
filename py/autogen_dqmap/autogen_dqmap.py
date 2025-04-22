5f54fdc3ead876e5e7cc4ef450eea67d0f28dd19
CPWang <chrisxp1223@gmail.com>
2025-04-22 15:59:29 +0800

    autogen_dqmap: add file header with GPL header
    add a GPL header, but needs check which opensource license should be
    used.
    
    TEST=NONE
    
    Signed-off-by: CPWang <chrisxp1223@gmail.com>

--- py/autogen_dqmap/autogen_dqmap.py
+++ py/autogen_dqmap/autogen_dqmap.py
@@ -1,3 +1,18 @@
+# Copyright 2024 CPWang and GPT]
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # User input excle file thne read the file
 # 1. excel file reading check
 #  - check the file is readable
@@ -5,10 +20,11 @@
 #  - check the data is valid
 
 
-# 2. validation base function 
+# 2. validation base function
 #  - check the if there any data is lost
 #  - check the if the namming is matched the requirement
 #  - check the pin number is in the avaliable range
+
 
 def generate_mem_data_groups(offsets, data_groups):
     result = []
